8ab2220ecd9f7ac139db34832e7cada7d6358e05
HelloCrossy <117866534+HelloCrossy@users.noreply.github.com>
2026-02-07 19:38:12 +0000

    Crossy gives up porting bio

--- src/main/java/com/github/hellocrossy/biologicalwonders/client/model/SeaBunnyModel.java
+++ src/main/java/com/github/hellocrossy/biologicalwonders/client/model/SeaBunnyModel.java
@@ -34,60 +34,70 @@ public class SeaBunnyModel extends SegmentedModel<SeaBunnyEntity> {
         this.texWidth = 32;
         this.texHeight = 32;
 
-        this.Body.addChild(this.Hips);
-        this.Hips.addChild(this.HipsTuft);
-        this.Ear2.addChild(this.EarTuft2);
-        this.Hips.addChild(this.Hips2);
-        this.Body.addChild(this.Head);
+
         this.Head.addChild(this.Ear1);
         this.Head.addChild(this.Head2);
-        this.HipsTuft.addChild(this.HipsTuft2);
-        this.Ear1.addChild(this.EarTuft1);
+        this.Body.addChild(this.Hips);
         this.Head.addChild(this.Ear2);
+        this.Body.addChild(this.Head);
+        this.Hips.addChild(this.Hips2);
+
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+        this.Hips = this.HipsTuft.getChild("Hips");
+        this.Ear1 = this.EarTuft1.getChild("Ear1");
+        this.HipsTuft = this.HipsTuft2.getChild("HipsTuft");
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+        this.Ear2 = this.EarTuft2.getChild("Ear2");
+
     }
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshDefinition = new MeshDefinition();
         PartDefinition partDefinition = meshDefinition.getRoot();
 
-        this.Hips = new ModelPart(this, 11, 9);
-        this.Hips.setPos(4.0F, 0.0F, 0.0F);
-        this.Hips.addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(Hips, 0.0F, 0.0F, 0.27366763203903305F);
-        this.HipsTuft = new ModelPart(this, 20, 10);
-        this.HipsTuft.setPos(0.0F, -1.8F, 0.0F);
-        this.HipsTuft.addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F);
         this.EarTuft2 = new ModelPart(this, 12, 19);
         this.EarTuft2.setPos(0.0F, 0.0F, 0.0F);
         this.EarTuft2.addBox(0.0F, -3.0F, -1.0F, 0.0F, 3.0F, 2.0F, 0.0F, 0.0F, 0.0F);
-        this.Hips2 = new ModelPart(this, 0, 12);
-        this.Hips2.setPos(-1.0F, 0.4F, 0.0F);
-        this.Hips2.addBox(0.0F, -1.1F, -1.5F, 3.0F, 3.0F, 3.0F, -0.01F, 0.0F, -0.01F);
-        this.setRotateAngle(Hips2, 0.0F, 0.0F, -0.27366763203903305F);
-        this.Head = new ModelPart(this, 9, 15);
-        this.Head.setPos(-3.9F, -0.3F, 0.0F);
-        this.Head.addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(Head, 0.0F, 0.0F, -0.1563815016444822F);
+        this.HipsTuft = new ModelPart(this, 20, 10);
+        this.HipsTuft.setPos(0.0F, -1.8F, 0.0F);
+        this.HipsTuft.addBox(-0.5F, -0.5F, -0.5F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F);
+        this.EarTuft1 = new ModelPart(this, 12, 19);
+        this.EarTuft1.setPos(0.0F, 0.0F, 0.0F);
+        this.EarTuft1.addBox(0.0F, -3.0F, -1.0F, 0.0F, 3.0F, 2.0F, 0.0F, 0.0F, 0.0F);
+        this.HipsTuft2 = new ModelPart(this, 18, 0);
+        this.HipsTuft2.setPos(0.0F, -0.8F, 0.0F);
+        this.HipsTuft2.addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.0F, 0.0F, 0.0F);
         this.Ear1 = new ModelPart(this, 0, 0);
         this.Ear1.setPos(0.6F, -1.3F, 0.9F);
         this.Ear1.addBox(-0.5F, -2.0F, -0.5F, 1.0F, 2.0F, 1.0F, 0.0F, 0.0F, 0.0F);
         this.setRotateAngle(Ear1, -0.23457224414434488F, 0.0F, 0.0F);
-        this.Body = new ModelPart(this, 0, 0);
-        this.Body.setPos(0.0F, 22.0F, 0.0F);
-        this.Body.addBox(-3.0F, -2.0F, -2.0F, 6.0F, 4.0F, 4.0F, 0.0F, 0.0F, 0.0F);
         this.Head2 = new ModelPart(this, 0, 18);
         this.Head2.setPos(1.5F, 1.6F, 0.0F);
         this.Head2.addBox(-3.0F, -1.0F, -1.5F, 3.0F, 2.0F, 3.0F, 0.0F, 0.0F, -0.01F);
         this.setRotateAngle(Head2, 0.0F, 0.0F, 0.1563815016444822F);
-        this.HipsTuft2 = new ModelPart(this, 18, 0);
-        this.HipsTuft2.setPos(0.0F, -0.8F, 0.0F);
-        this.HipsTuft2.addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 2.0F, 0.0F, 0.0F, 0.0F);
-        this.EarTuft1 = new ModelPart(this, 12, 19);
-        this.EarTuft1.setPos(0.0F, 0.0F, 0.0F);
-        this.EarTuft1.addBox(0.0F, -3.0F, -1.0F, 0.0F, 3.0F, 2.0F, 0.0F, 0.0F, 0.0F);
+        this.Hips = new ModelPart(this, 11, 9);
+        this.Hips.setPos(4.0F, 0.0F, 0.0F);
+        this.Hips.addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, 0.0F, 0.0F, 0.0F);
+        this.setRotateAngle(Hips, 0.0F, 0.0F, 0.27366763203903305F);
         this.Ear2 = new ModelPart(this, 0, 0);
         this.Ear2.setPos(0.6F, -1.3F, -0.9F);
         this.Ear2.addBox(-0.5F, -2.0F, -0.5F, 1.0F, 2.0F, 1.0F, 0.0F, 0.0F, 0.0F);
         this.setRotateAngle(Ear2, 0.23457224414434488F, 0.0F, 0.0F);
+        this.Head = new ModelPart(this, 9, 15);
+        this.Head.setPos(-3.9F, -0.3F, 0.0F);
+        this.Head.addBox(-1.5F, -1.5F, -1.5F, 3.0F, 3.0F, 3.0F, 0.0F, 0.0F, 0.0F);
+        this.setRotateAngle(Head, 0.0F, 0.0F, -0.1563815016444822F);
+        this.Hips2 = new ModelPart(this, 0, 12);
+        this.Hips2.setPos(-1.0F, 0.4F, 0.0F);
+        this.Hips2.addBox(0.0F, -1.1F, -1.5F, 3.0F, 3.0F, 3.0F, -0.01F, 0.0F, -0.01F);
+        this.setRotateAngle(Hips2, 0.0F, 0.0F, -0.27366763203903305F);
+        this.Body = new ModelPart(this, 0, 0);
+        this.Body.setPos(0.0F, 22.0F, 0.0F);
+        this.Body.addBox(-3.0F, -2.0F, -2.0F, 6.0F, 4.0F, 4.0F, 0.0F, 0.0F, 0.0F);
+        this.setRotateAngle(Body, 0.0F, -1.5774285300155755F, 0.0F);
 
         return LayerDefinition.create(meshDefinition, texWidth, texHeight);
     }
