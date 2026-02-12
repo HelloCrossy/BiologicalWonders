583c6b42173df69be4fbd25b7468acd66db89b51
Mnesikos <mnesikos@gmail.com>
2026-02-12 15:37:49 -0500

    1.20 items changes

--- src/main/java/com/github/hellocrossy/biologicalwonders/BiologicalWonders.java
+++ src/main/java/com/github/hellocrossy/biologicalwonders/BiologicalWonders.java
@@ -1,28 +1,43 @@
 package com.github.hellocrossy.biologicalwonders;
 
-
 import com.github.hellocrossy.biologicalwonders.entity.BioEntities;
 import com.github.hellocrossy.biologicalwonders.item.BioItems;
 import com.github.hellocrossy.biologicalwonders.sounds.BioSounds;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.zawamod.zawa.Zawa;
 
 @Mod(BiologicalWonders.MOD_ID)
 public class BiologicalWonders {
     public static final String MOD_ID = "biologicalwonders";
+//    public static final String[] PLUSHIES_LIST = new String[]{};
 
     public BiologicalWonders() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         BioEntities.REGISTRY.initialize();
-        BioItems.REGISTRAR.register(bus);
+        BioItems.ITEMS_REGISTRAR.register(bus);
         BioSounds.REGISTRAR.register(bus);
 
+        bus.addListener(this::addCreativeTabs);
         bus.addListener(this::setup);
         bus.addListener(this::setupClient);
+    }
+
+    private void addCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == Zawa.ITEMS_GROUP.getKey()) {
+            BioItems.ITEMS_REGISTRAR.getEntries().forEach(item -> event.accept(item.get()));
+        }
+        if (event.getTabKey() == Zawa.DECORATIONS_GROUP.getKey()) {
+            BioItems.DECORATIONS_REGISTRAR.getEntries().forEach(item -> event.accept(item.get()));
+        }
+        if (event.getTabKey() == Zawa.ENTITIES_GROUP.getKey()) {
+            BioEntities.REGISTRY.itemRegistrar.getEntries().forEach(item -> event.accept(item.get()));
+        }
     }
 
     private void setup(final FMLCommonSetupEvent event) {
--- src/main/java/com/github/hellocrossy/biologicalwonders/item/BioItems.java
+++ src/main/java/com/github/hellocrossy/biologicalwonders/item/BioItems.java
@@ -2,92 +2,86 @@ package com.github.hellocrossy.biologicalwonders.item;
 
 import com.github.hellocrossy.biologicalwonders.BiologicalWonders;
 import com.github.hellocrossy.biologicalwonders.entity.BioEntities;
-import net.minecraft.Util;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.zawamod.zawa.Zawa;
-import org.zawamod.zawa.world.item.PlushBlockItem;
+import net.minecraftforge.registries.RegistryObject;
+import org.zawamod.zawa.world.item.ZawaBucketItem;
 import org.zawamod.zawa.world.item.ZawaEggItem;
-import org.zawamod.zawa.world.item.ZawaFishBucketItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.github.hellocrossy.biologicalwonders.BiologicalWonders.PLUSHIES_LIST;
-
+//import static com.github.hellocrossy.biologicalwonders.BiologicalWonders.PLUSHIES_LIST;
 
 public class BioItems {
-    public static final DeferredRegister<Item> REGISTRAR = DeferredRegister.create(ForgeRegistries.ITEMS, BiologicalWonders.MOD_ID);
-    public static final List<RegistryObject<Item>> PLUSHIES = Util.make(new ArrayList<>(), list -> {
-        for (String plush : PLUSHIES_LIST)
-            list.add(REGISTRAR.register(plush + "_plush", () -> new PlushBlockItem(BioBlocks.PLUSHIES.get(plush).get(), new Item.Properties().tab(Zawa.DECORATIONS_GROUP))));
-    });
+    public static final DeferredRegister<Item> ITEMS_REGISTRAR = DeferredRegister.create(ForgeRegistries.ITEMS, BiologicalWonders.MOD_ID);
+    public static final DeferredRegister<Item> DECORATIONS_REGISTRAR = DeferredRegister.create(ForgeRegistries.ITEMS, BiologicalWonders.MOD_ID);
+//    public static final List<RegistryObject<Item>> PLUSHIES = Util.make(new ArrayList<>(), list -> {
+//        for (String plush : PLUSHIES_LIST)
+//            list.add(REGISTRAR.register(plush + "_plush", () -> new PlushBlockItem(BioBlocks.PLUSHIES.get(plush).get(), new Item.Properties().tab(Zawa.DECORATIONS_GROUP))));
+//    });
 
-    public static final RegistryObject<Item> CUBAN_CROCODILE_EGG = REGISTRAR.register("cuban_crocodile_egg", () -> new ZawaEggItem(BioEntities.CUBAN_CROCODILE, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
-
-    public static final RegistryObject<Item>
-            BUTTERFLYFISH = REGISTRAR.register("butterflyfish", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
+    public static final RegistryObject<Item> CUBAN_CROCODILE_EGG = ITEMS_REGISTRAR.register("cuban_crocodile_egg", () -> new ZawaEggItem(BioEntities.CUBAN_CROCODILE, new Item.Properties().stacksTo(16)));
 
     public static final RegistryObject<Item>
-            COWFISH = REGISTRAR.register("cowfish", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item>
-            BUTTERFLYFISH_BUCKET = REGISTRAR.register("bucket_of_butterflyfish", () -> new ZawaFishBucketItem(BioEntities.BUTTERFLYFISH, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            BUTTERFLYFISH = ITEMS_REGISTRAR.register("butterflyfish", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item>
-            COWFISH_BUCKET = REGISTRAR.register("bucket_of_cowfish", () -> new ZawaFishBucketItem(BioEntities.COWFISH, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            COWFISH = ITEMS_REGISTRAR.register("cowfish", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>
+            BUTTERFLYFISH_BUCKET = ITEMS_REGISTRAR.register("bucket_of_butterflyfish", () -> new ZawaBucketItem(BioEntities.BUTTERFLYFISH, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            HORSESHOE_CRAB = REGISTRAR.register("horseshoe_crab", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
+            COWFISH_BUCKET = ITEMS_REGISTRAR.register("bucket_of_cowfish", () -> new ZawaBucketItem(BioEntities.COWFISH, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            MOORISH_IDOL = REGISTRAR.register("moorish_idol", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item>
-            HORSESHOE_CRAB_BUCKET = REGISTRAR.register("bucket_of_horseshoe_crab", () -> new ZawaFishBucketItem(BioEntities.HORSESHOE_CRAB, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            HORSESHOE_CRAB = ITEMS_REGISTRAR.register("horseshoe_crab", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item>
-            MOORISH_IDOL_BUCKET = REGISTRAR.register("bucket_of_moorish_idol", () -> new ZawaFishBucketItem(BioEntities.MOORISH_IDOL, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            MOORISH_IDOL = ITEMS_REGISTRAR.register("moorish_idol", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>
+            HORSESHOE_CRAB_BUCKET = ITEMS_REGISTRAR.register("bucket_of_horseshoe_crab", () -> new ZawaBucketItem(BioEntities.HORSESHOE_CRAB, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            SEA_BUNNY = REGISTRAR.register("sea_bunny", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
+            MOORISH_IDOL_BUCKET = ITEMS_REGISTRAR.register("bucket_of_moorish_idol", () -> new ZawaBucketItem(BioEntities.MOORISH_IDOL, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            SPOTTED_DRUM = REGISTRAR.register("spotted_drum", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item>
-            SPOTTED_DRUM_BUCKET = REGISTRAR.register("bucket_of_spotted_drum", () -> new ZawaFishBucketItem(BioEntities.SPOTTED_DRUM, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            SEA_BUNNY = ITEMS_REGISTRAR.register("sea_bunny", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item>
-            SEA_BUNNY_BUCKET = REGISTRAR.register("bucket_of_sea_bunny", () -> new ZawaFishBucketItem(BioEntities.SEA_BUNNY, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            SPOTTED_DRUM = ITEMS_REGISTRAR.register("spotted_drum", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>
+            SPOTTED_DRUM_BUCKET = ITEMS_REGISTRAR.register("bucket_of_spotted_drum", () -> new ZawaBucketItem(BioEntities.SPOTTED_DRUM, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            SQUIRRELFISH = REGISTRAR.register("squirrelfish", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
+            SEA_BUNNY_BUCKET = ITEMS_REGISTRAR.register("bucket_of_sea_bunny", () -> new ZawaBucketItem(BioEntities.SEA_BUNNY, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            TRIGGERFISH = REGISTRAR.register("triggerfish", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item>
-            SQUIRRELFISH_BUCKET = REGISTRAR.register("bucket_of_squirrelfish", () -> new ZawaFishBucketItem(BioEntities.SQUIRRELFISH, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            SQUIRRELFISH = ITEMS_REGISTRAR.register("squirrelfish", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item>
-            TRIGGERFISH_BUCKET = REGISTRAR.register("bucket_of_triggerfish", () -> new ZawaFishBucketItem(BioEntities.TRIGGERFISH, () -> Fluids.WATER,
-            new Item.Properties().stacksTo(1).tab(Zawa.ITEMS_GROUP)));
+            TRIGGERFISH = ITEMS_REGISTRAR.register("triggerfish", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item>
+            SQUIRRELFISH_BUCKET = ITEMS_REGISTRAR.register("bucket_of_squirrelfish", () -> new ZawaBucketItem(BioEntities.SQUIRRELFISH, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item>
-            TULIP_SNAIL = REGISTRAR.register("tulip_snail", () -> new Item(new Item.Properties().tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item> ALLIGATOR_GAR_EGG = REGISTRAR.register("alligator_gar_egg", () -> new ZawaEggItem(BioEntities.ALLIGATOR_GAR, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item> BLACKTIP_SHARK_EGG = REGISTRAR.register("blacktip_shark_egg", () -> new ZawaEggItem(BioEntities.BLACKTIP_SHARK, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item> COWNOSE_RAY_EGG = REGISTRAR.register("cownose_ray_egg", () -> new ZawaEggItem(BioEntities.COWNOSE_RAY, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item> NURSE_SHARK_EGG = REGISTRAR.register("nurse_shark_egg", () -> new ZawaEggItem(BioEntities.NURSE_SHARK, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item> NAPOLEON_WRASSE_EGG = REGISTRAR.register("napoleon_wrasse_egg", () -> new ZawaEggItem(BioEntities.NAPOLEON_WRASSE, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
-    public static final RegistryObject<Item> LIONFISH_EGG = REGISTRAR.register("lionfish_egg", () -> new ZawaEggItem(BioEntities.LIONFISH, new Item.Properties().stacksTo(16).tab(Zawa.ITEMS_GROUP)));
+            TRIGGERFISH_BUCKET = ITEMS_REGISTRAR.register("bucket_of_triggerfish", () -> new ZawaBucketItem(BioEntities.TRIGGERFISH, () -> Fluids.WATER, () -> SoundEvents.BUCKET_EMPTY_FISH,
+            new Item.Properties().stacksTo(1)));
+
+    public static final RegistryObject<Item> TULIP_SNAIL = ITEMS_REGISTRAR.register("tulip_snail", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ALLIGATOR_GAR_EGG = ITEMS_REGISTRAR.register("alligator_gar_egg", () -> new ZawaEggItem(BioEntities.ALLIGATOR_GAR, new Item.Properties().stacksTo(16)));
+    public static final RegistryObject<Item> BLACKTIP_SHARK_EGG = ITEMS_REGISTRAR.register("blacktip_shark_egg", () -> new ZawaEggItem(BioEntities.BLACKTIP_SHARK, new Item.Properties().stacksTo(16)));
+    public static final RegistryObject<Item> COWNOSE_RAY_EGG = ITEMS_REGISTRAR.register("cownose_ray_egg", () -> new ZawaEggItem(BioEntities.COWNOSE_RAY, new Item.Properties().stacksTo(16)));
+    public static final RegistryObject<Item> NURSE_SHARK_EGG = ITEMS_REGISTRAR.register("nurse_shark_egg", () -> new ZawaEggItem(BioEntities.NURSE_SHARK, new Item.Properties().stacksTo(16)));
+    public static final RegistryObject<Item> NAPOLEON_WRASSE_EGG = ITEMS_REGISTRAR.register("napoleon_wrasse_egg", () -> new ZawaEggItem(BioEntities.NAPOLEON_WRASSE, new Item.Properties().stacksTo(16)));
+    public static final RegistryObject<Item> LIONFISH_EGG = ITEMS_REGISTRAR.register("lionfish_egg", () -> new ZawaEggItem(BioEntities.LIONFISH, new Item.Properties().stacksTo(16)));
 
 }
 
