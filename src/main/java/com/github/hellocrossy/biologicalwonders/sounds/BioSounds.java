fcb1f748dc6798d135f8d0cddbee94af137807d5
Mnesikos <mnesikos@gmail.com>
2026-02-12 16:12:30 -0500

    1.20 sounds changes

--- src/main/java/com/github/hellocrossy/biologicalwonders/sounds/BioSounds.java
+++ src/main/java/com/github/hellocrossy/biologicalwonders/sounds/BioSounds.java
@@ -1,16 +1,16 @@
 package com.github.hellocrossy.biologicalwonders.sounds;
 
 import com.github.hellocrossy.biologicalwonders.BiologicalWonders;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class BioSounds {
     public static final DeferredRegister<SoundEvent> REGISTRAR = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, BiologicalWonders.MOD_ID);
 
     private static RegistryObject<SoundEvent> registerSound(String name) {
-        return REGISTRAR.register(name, () -> new SoundEvent(new ResourceLocation(BiologicalWonders.MOD_ID, name)));
+        return REGISTRAR.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(BiologicalWonders.MOD_ID, name)));
     }
 }
